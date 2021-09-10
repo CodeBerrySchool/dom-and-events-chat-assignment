33d75ef97200ba122bea7263ff31f86b6f0d7b34
starlord0302 <karoly0302@gmail.com>
2021-09-10 15:33:03 +0200

    add project

---
+++ chat.js
@@ -0,0 +1,11 @@
+let nameInput = document.querySelector('.name-form__input');
+let nameForm = document.querySelector('.nav__name-form');
+let greetingDiv = document.querySelector('.main__greeting');
+
+function getName(event) {
+  event.preventDefault();
+  let name = nameInput.value;
+  console.log(name);
+}
+
+nameForm.addEventListener('submit', getName);
